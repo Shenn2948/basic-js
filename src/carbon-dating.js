6bad5ec2aa30cee5b9480a802d513966ca1a25ef
Shenn <shenn2948@gmail.com>
2020-09-10 10:38:38 +0300

    feat: added carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+/**
+ *
+ * @param {String} sampleActivity
+ */
+module.exports = function dateSample(sampleActivity) {
+
+  if (typeof (sampleActivity) !== 'string') {
+    return false;
+  }
+
+  const activity = parseFloat(sampleActivity);
+  if (isNaN(activity) || activity > 15) {
+    return false;
+  }
+
+  const ln2 = 0.693;
+  const rateConstant = ln2 / HALF_LIFE_PERIOD;
+  const age = Math.log(MODERN_ACTIVITY / activity) / rateConstant;
+
+  const result = Math.ceil(age);
+  return isFinite(result) ? result : false;
 };
