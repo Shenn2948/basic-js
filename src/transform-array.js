af373beb68973ade314021bd059b4abe8a83216d
Andrei Kazheunikau <shenn2948@gmail.com>
2020-12-01 21:14:48 +0300

    upd

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+/**
+ *
+ * @param {Array} arr
+ */
+module.exports = function transform(arr) {
+  const doubles = arr.map((value, index, array) => {
+    const prev = array[index - 1];
+    const next = array[index + 1];
+
+    if (value === `--double-prev`) {
+      return prev;
+    }
+
+    if (value === `--double-next`) {
+      return next;
+    }
+
+    if (next === `--discard-prev` || prev === `--discard-next`) {
+      return null;
+    }
+
+    return value;
+
+  }).filter(x => x !== null && x !== undefined && x !== `--discard-prev` && x !== `--discard-next`);
+
+  return doubles;
 };
